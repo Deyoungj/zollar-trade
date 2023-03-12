a04aa8252acf414c43c3463c521832a754ae952d
Deyoungj <emmanuelonyibor39@gmail.com>
2023-03-12 20:33:37 +0100

    creating custom user profile

--- user/custom_manager.py
+++ user/custom_manager.py
@@ -32,7 +32,7 @@ class CustomUserManager(BaseUserManager):
         if not email:
             raise ValueError("Email Required")
 
-        email = self.normalize_email(email)
+        email = self.normalize_email(email)      
         user = self.model(email=email, **extra_fields)
         user.set_password(password)
         user.save()
--- user/models.py
+++ user/models.py
@@ -1,15 +1,41 @@
 from django.db import models
 
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
-
+from .custom_manager import CustomUserManager
 
 class CustomUser(AbstractBaseUser, PermissionsMixin):
     full_name = models.CharField(max_length=100, blank=True)
     username = models.CharField(max_length=100, unique=True)
     email = models.EmailField(unique=True)
-    phone_number = models.CharField(max_length=20)
-    address = models.CharField(max_length=150)
-    contry = models.CharField(max_length=150)
-    BTC_Wallet_Address = models.CharField(max_length=200)
-    Ethereum_Bep20_Address = models.CharField(max_length=200)
-    Tether_USDT_TRC20 = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+
+    USERNAME_FIELD = "email"
+
+    objeet = CustomUserManager
+    
+    def __str__(self) -> str:
+        return self.full_name
+
+    class Meta:
+        ordering = ('-created_at',)
+
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    image = models.ImageField(default="default.jpg")
+    phone_number = models.CharField(max_length=20, blank=True)
+    address = models.CharField(max_length=150, blank=True)
+    contry = models.CharField(max_length=150, blank=True)
+    BTC_Wallet_Address = models.CharField(max_length=200, blank=True)
+    Ethereum_Bep20_Address = models.CharField(max_length=200, blank=True)
+    Tether_USDT_TRC20 = models.CharField(max_length=200, blank=True)
+
+    def __str__(self) -> str:
+        return self.user
+
+
+
+
+
