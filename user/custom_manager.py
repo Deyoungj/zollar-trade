910ca7bde56a1df438de549d53e4c493699e8673
Deyoungj <emmanuelonyibor39@gmail.com>
2023-03-12 12:24:02 +0100

    creating custom user account

---
+++ user/custom_manager.py
@@ -0,0 +1,40 @@
+from django.contrib.auth.models import BaseUserManager
+
+
+class CustomUserManager(BaseUserManager):
+
+    def create_user(self, email, password, **extra_fields):
+
+        if not email:
+            raise ValueError("Email Required")
+        
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+
+        return user
+
+
+    def create_superuser(self, email, password, **extra_fields):
+
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError("is_staff must be set to true")
+        
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError("is_superuser must be set to true")
+        
+
+        if not email:
+            raise ValueError("Email Required")
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+
+        return user
--- user/models.py
+++ user/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    full_name = models.CharField(max_length=100, blank=True)
+    username = models.CharField(max_length=100, unique=True)
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=20)
+    address = models.CharField(max_length=150)
+    contry = models.CharField(max_length=150)
+    BTC_Wallet_Address = models.CharField(max_length=200)
+    Ethereum_Bep20_Address = models.CharField(max_length=200)
+    Tether_USDT_TRC20 = models.CharField(max_length=200)
