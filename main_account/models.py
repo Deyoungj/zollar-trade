0cd39b790ae5667a1400e0f39fe6d68117f1276e
Deyoungj <emmanuelonyibor39@gmail.com>
2023-03-16 22:17:06 +0100

    added account app

--- main_account/models.py
+++ main_account/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from user.models import CustomUser
 
-# Create your models here.
+
+class Account(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    account_ballance = models.DecimalField(max_digits=8, decimal_places=2)
+    total_profit = models.DecimalField(max_digits=8, decimal_places=2)
+    active_deposit = models.DecimalField(max_digits=8, decimal_places=2)
+
+
+    def __str__(self):
+        return self.user.full_name
